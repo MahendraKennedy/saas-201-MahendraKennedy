fa90299fe284bdfd1d4627903ef875e518e65b06
Mahendra Kennedy <121003161@sastra.ac.in>
2019-11-01 19:56:50 +0530

    Adding our submission for the final project

---
+++ final-project/proj_blog/app/mailers/contact_mailer.rb
@@ -0,0 +1,16 @@
+class ContactMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.contact_mailer.contact.subject
+  #
+
+  default from: ENV['username_from_mail']#'121003161@sastra.ac.in'
+
+  def contact(message, to)
+    @message = message
+    @to = to
+    mail(to: @to, subject: 'Activity on BlogPost Project Community')
+  end
+end
